59171e4ce93a705a4e6bfbab671712b799caae1a
GatsbyNewton <wangzmking@gmail.com>
2021-05-17 00:25:25 +0800

    parameter

--- src/main/java/edu/wzm/param/WordCountParam.java
+++ src/main/java/edu/wzm/param/WordCountParam.java
@@ -1,12 +1,40 @@
 package edu.wzm.param;
 
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.*;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class WordCount {
+import java.util.Arrays;
+
+/**
+ * <p>Usage: <code>WordCountParam --input=&lt;path&gt; --output=&lt;path&gt; --operator=&lt;operator&gt;</code><br>
+ */
+public class WordCountParam {
+    private static final Logger LOGGER = LogManager.getLogger(WordCountParam.class);
     public static void main(String[] args) {
         WordCountOptions options = PipelineOptionsFactory.fromArgs(args)
                 .withValidation()
                 .as(WordCountOptions.class);
+        LOGGER.error("The Operator: {}", options.getOperator());
+
+        Pipeline pipeline = Pipeline.create(options);
+        pipeline.apply(TextIO.read().from(options.getInput()))
+                .apply(MapElements.into(TypeDescriptors.lists(TypeDescriptors.strings()))
+                        .via(str -> Arrays.asList(str.split("\\W+"))))
+                .apply(Flatten.iterables())
+                .apply(Count.perElement())
+                .apply(MapElements.into(TypeDescriptor.of(String.class))
+                        .via(kv -> String.format("%s: %d", kv.getKey(), kv.getValue())))
+                .apply(TextIO.write().to(options.getOutput()));
+
+        pipeline.run().waitUntilFinish();
     }
 
     public interface WordCountOptions extends PipelineOptions {
@@ -16,9 +44,9 @@ public class WordCount {
          */
         @Description("Path of the file to read from")
         @Default.String("gs://apache-beam-samples/shakespeare/kinglear.txt")
-        String getInputFile();
+        String getInput();
 
-        void setInputFile(String value);
+        void setInput(String value);
 
         /** Set this required option to specify where to write the output. */
         @Description("Path of the file to write to")
@@ -26,5 +54,10 @@ public class WordCount {
         String getOutput();
 
         void setOutput(String value);
+
+        @Default.String("Operator")
+        String getOperator();
+
+        void setOperator(String value);
     }
 }
